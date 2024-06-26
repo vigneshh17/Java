abb8cc3de3d6ecdd73972e24ba5c8cdfdbeda1d2
Vignesh <vigneshapppu978@gmail.com>
2024-06-26 22:44:37 +0530

    first commit

---
+++ Day-2/Datatypes_Bytes.java
@@ -0,0 +1,8 @@
+public class Datatypes_Bytes{
+public static void main(String args[]) {
+byte b=127;
+char a= '\u0055';
+System.out.println(a);
+System.out.println(b);
+}}
+
---
+++ Day-2/Loaders.java
@@ -0,0 +1,10 @@
+import java.sql.DriverManager;
+import java.util.Scanner;
+
+class Loaders {
+
+    public void run() {}
+    public static void main(String args[]) {
+      System.out.println(Loaders.class.getm);
+    }
+}
