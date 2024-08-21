662e8219d9c9fb8afbaff373844304521f733655
Vignesh <vigneshapppu978@gmail.com>
2024-08-21 22:00:38 +0530

    jjj

--- Day-24/Node1.java
+++ Day-24/Node1.java
@@ -5,10 +5,13 @@ public class Node1 {
     Node node3= new Node(40);
     Node node4= new Node(50);
     Node node5= new Node(60);
+    Node node6=new Node(1000);
     node1.next=node2;
-    node2.next=node3;
-    node3.next=node4;
-    node4.next=node5;
+    node2.next=node6;
+    node3.next=node6;
+    node6.next=node4;
+    node4.next = node5;
+
     while (node1 != null) {
      System.out.println(node1.data);
      node1 =node1.next;
